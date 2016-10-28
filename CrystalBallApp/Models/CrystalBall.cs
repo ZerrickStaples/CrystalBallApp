da7d2a113ca567d8c4855582546ab705da56336b
zerrickstaples <zerrick.staples@gmail.com>
2016-10-28 08:45:32 -0400

    added files

---
+++ CrystalBallApp/Models/CrystalBall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrystalBallApp.Models
+{
+    public class CrystalBall
+    {
+        //get random number
+        //create an array of strings for responses
+
+        public string getResponse()
+        {
+            string[] responses = { "Hey", "What's up", "Hello" };
+            //get random number object
+            Random randomNum = new Random();
+            var num = randomNum.Next(0, responses.Length);
+            return responses[num];
+
+        }
+    }
+}
